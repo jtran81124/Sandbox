e177a4cca11ab4a7db411e0ec3404c813e470580
Jacky Tran <jtran81124@gmail.com>
2020-06-25 11:52:26 -0700

    try 3

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ print("Save, commit, push")
 print("practicing pushing more onto master")
 print("practice pulling from master")
 print("practice working only in master")
+
+print("second try of working only in master")

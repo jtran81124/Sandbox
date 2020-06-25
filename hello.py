da98b386167ceeca3e90c3673489052f65cdc2c6
Jacky Tran <jtran81124@gmail.com>
2020-06-25 11:56:00 -0700

    try 4

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ print("Save, commit, push")
 print("practicing pushing more onto master")
 print("practice pulling from master")
 print("practice working in a clone environment")
+
+print("second try at working in only a branch")
